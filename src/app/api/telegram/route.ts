1a25b34617f10db170edaea48c17c601bf7633ce
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2024-08-02 11:59:52 +0400

    upd.

--- src/app/api/telegram/route.ts
+++ src/app/api/telegram/route.ts
@@ -27,31 +27,41 @@ export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
         const message: Message = body.message;
+        let limit = 1;
 
         await logWebhookInfo();
 
         if (message && message.text && message.text.startsWith('/start')) {
             const chatId = message.chat.id;
-            const firstName = message.chat.first_name || 'User';
+            const firstName = message.chat.first_name;
 
-            await fetch(`https://api.telegram.org/bot${telegram_bot_token}/sendMessage`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    chat_id: chatId,
-                    text: `Здравстуйте, ${firstName}! Чтобы подключить уведомления от приложения Harmonysub пришлите имя пользователя, которые Вы указывали при регистрации в приложении`,
-                }),
-            });
+            if (limit == 1) {
+                await fetch(`https://api.telegram.org/bot${telegram_bot_token}/sendMessage`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        chat_id: chatId,
+                        text: `Здравстуйте, ${firstName}! Чтобы подключить уведомления от приложения Harmonysub пришлите имя пользователя, которые Вы указывали при регистрации в приложении`,
+                    }),
+                })
+                    .then((res) => console.log(res.json()));
+                limit = 0;
+
+                return NextResponse.json({ message: 'Пользователь отправил старт боту' });
+            } else if (limit == 0) {
+                limit = 1;
+                return NextResponse.json({ message: 'Бот уже ответил' });
+            }
         } else if (message && message.text) {
             const chatId = message.chat.id;
             const username = message.text;
             const telegram_username = message.chat.username;
 
             const existingUser = await sql`
-                SELECT * FROM users WHERE username = ${username}
+                SELECT * FROM users WHERE telegram_chat_id = ${chatId}
             `;
 
-            if (existingUser.rows.length == null) {
+            if (existingUser.rows.length == 0) {
                 await sql`
                     INSERT INTO users (telegram_chat_id, telegram_username)
                     VALUES (${chatId}, ${telegram_username})
@@ -61,27 +71,38 @@ export async function POST(req: NextRequest) {
                     WHERE users.username = ${username}
                 `;
 
-                await fetch(`https://api.telegram.org/bot${telegram_bot_token}/sendMessage`, {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        chat_id: chatId,
-                        text: `Вы успешно подключили уведомления от приложения Harmonysub!`,
-                    }),
-                });
+                if (limit == 1) {
+                    await fetch(`https://api.telegram.org/bot${telegram_bot_token}/sendMessage`, {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                            chat_id: chatId,
+                            text: `Вы успешно подключили уведомления от приложения Harmonysub!`,
+                        }),
+                    });
 
-                return NextResponse.json({ message: 'Already connected' });
+                    limit = 0;
+                    return NextResponse.json({ message: 'Пользователь успешно подключил телеграм к приложению' });
+                } else if (limit == 0) {
+                    limit = 1;
+                    return NextResponse.json({ message: 'Бот уже ответил' });
+                }
             } else if (existingUser.rows.length > 0) {
-                await fetch(`https://api.telegram.org/bot${telegram_bot_token}/sendMessage`, {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({
-                        chat_id: chatId,
-                        text: `Вы уже подключили уведомления от приложения Harmonysub!`,
-                    }),
-                });
+                if (limit == 1) {
+                    await fetch(`https://api.telegram.org/bot${telegram_bot_token}/sendMessage`, {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                            chat_id: chatId,
+                            text: `Вы уже подключили уведомления от приложения Harmonysub!`,
+                        }),
+                    });
 
-                return NextResponse.json({ message: 'Already connected' });
+                    return NextResponse.json({ message: 'Пользователь уже подключил телеграм к приложению' });
+                } else if (limit == 0) {
+                    limit = 1;
+                    return NextResponse.json({ message: 'Бот уже ответил' });
+                }
             }
         } else {
             console.error('Invalid message format:', message);
