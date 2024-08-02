d8aa2ab044a161d29665f78ae73086dd19090dfc
Vladimir Mishenskii <60189954+vovamishenskiy@users.noreply.github.com>
2024-08-02 16:45:02 +0400

    правка проверки подключения на клиенте

--- src/components/ConnectTelegramButton.tsx
+++ src/components/ConnectTelegramButton.tsx
@@ -42,20 +42,20 @@ const ConnectTelegramButton = () => {
                 <div className="flex flex-col">
                     <div className="flex flex-row gap-2">
                         <XCircleIcon className="w-6 h-6 mb-3" />
-                        <p>Уведомления от телеграм бота отключены</p>
+                        <p>Уведомления от телеграм бота подключены</p>
                     </div>
                     <button onClick={handleConnect} className="btn btn-primary rounded-xl w-2/3 p-2 bg-emerald-700 hover:bg-emerald-600 text-white transition ease">
-                        Подключить уведомления
+                        Отключить уведомления
                     </button>
                 </div>
             ) : (
                 <div className="flex flex-col">
                     <div className="flex flex-row gap-2">
                         <CheckCircleIcon className="w-6 h-6 mb-3" />
-                        <p>Уведомления от телеграм бота подключены</p>
+                        <p>Уведомления от телеграм бота отключены</p>
                     </div>
                     <button onClick={handleDisconnect} className="btn btn-primary rounded-xl w-2/3 p-2 bg-emerald-700 hover:bg-emerald-600 text-white transition ease">
-                        Отключить уведомления
+                        Подключить уведомления
                     </button>
                 </div>
             )}
